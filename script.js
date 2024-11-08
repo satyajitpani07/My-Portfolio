0e28225b2222116ba849de53529cb792f95848f3
satyajitpani07 <satyajitpani2000@gmail.com>
2024-11-08 11:30:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let body=document.getElementById('s')
+
+function change()
+{
+  if(body.style.backgroundColor!=black){
+    body.style.backgroundColor="black";
+    body.style.color="white";
+  }else{
+    body.style.backgroundColor="white";
+    body.style.color="black";
+  }
+}
